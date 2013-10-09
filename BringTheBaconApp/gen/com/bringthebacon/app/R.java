568fe30bba845e4374bdfff04fd4c4d3a8088aa2
Joshua Marner <marner2@illinois.edu>
2013-10-09 10:12:27 -0500

    Added a simple Contribution list/detail view for our syncronized list.

--- BringTheBaconApp/gen/com/bringthebacon/app/R.java
+++ BringTheBaconApp/gen/com/bringthebacon/app/R.java
@@ -24,38 +24,62 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f080007;
-        public static final int email=0x7f080003;
-        public static final int login=0x7f080005;
-        public static final int login_form=0x7f080002;
-        public static final int login_status=0x7f080000;
-        public static final int login_status_message=0x7f080001;
-        public static final int password=0x7f080004;
-        public static final int sign_in_button=0x7f080006;
+        public static final int action_forgot_password=0x7f08000a;
+        public static final int contribution_detail=0x7f080009;
+        public static final int contribution_detail_container=0x7f080000;
+        public static final int contribution_list=0x7f080001;
+        public static final int email=0x7f080005;
+        public static final int login=0x7f080007;
+        public static final int login_form=0x7f080004;
+        public static final int login_status=0x7f080002;
+        public static final int login_status_message=0x7f080003;
+        public static final int password=0x7f080006;
+        public static final int sign_in_button=0x7f080008;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
+        public static final int activity_contribution_detail=0x7f030000;
+        /** 
+    Layout alias to replace the single-pane version of the layout with a
+    two-pane version on Large screens.
+
+    For more on layout aliases, see:
+    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
+    
+
+    Layout alias to replace the single-pane version of the layout with a
+    two-pane version on Large screens.
+
+    For more on layout aliases, see:
+    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
+    
+         */
+        public static final int activity_contribution_list=0x7f030001;
+        public static final int activity_contribution_twopane=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int fragment_contribution_detail=0x7f030004;
     }
     public static final class menu {
         public static final int login=0x7f070000;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f050008;
+        public static final int action_forgot_password=0x7f05000a;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050006;
-        public static final int action_sign_in_short=0x7f050007;
+        public static final int action_sign_in_register=0x7f050008;
+        public static final int action_sign_in_short=0x7f050009;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f05000d;
-        public static final int error_incorrect_password=0x7f05000c;
-        public static final int error_invalid_email=0x7f05000a;
-        public static final int error_invalid_password=0x7f05000b;
+        public static final int error_field_required=0x7f05000f;
+        public static final int error_incorrect_password=0x7f05000e;
+        public static final int error_invalid_email=0x7f05000c;
+        public static final int error_invalid_password=0x7f05000d;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f050009;
+        public static final int login_progress_signing_in=0x7f05000b;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050004;
-        public static final int prompt_password=0x7f050005;
-        public static final int title_activity_login=0x7f050003;
+        public static final int prompt_email=0x7f050006;
+        public static final int prompt_password=0x7f050007;
+        public static final int title_activity_login=0x7f050005;
+        public static final int title_contribution_detail=0x7f050004;
+        public static final int title_contribution_list=0x7f050003;
     }
     public static final class style {
         /** 
---
+++ BringTheBaconApp/src/com/bringthebacon/app/ContributionDetailFragment.java
@@ -0,0 +1,63 @@
+package com.bringthebacon.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bringthebacon.app.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Contribution detail screen. This fragment is
+ * either contained in a {@link ContributionListActivity} in two-pane mode (on
+ * tablets) or a {@link ContributionDetailActivity} on handsets.
+ */
+public class ContributionDetailFragment extends Fragment {
+	/**
+	 * The fragment argument representing the item ID that this fragment
+	 * represents.
+	 */
+	public static final String ARG_ITEM_ID = "item_id";
+
+	/**
+	 * The dummy content this fragment is presenting.
+	 */
+	private DummyContent.DummyItem mItem;
+
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public ContributionDetailFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (getArguments().containsKey(ARG_ITEM_ID)) {
+			// Load the dummy content specified by the fragment
+			// arguments. In a real-world scenario, use a Loader
+			// to load content from a content provider.
+			mItem = DummyContent.ITEM_MAP.get(getArguments().getString(
+					ARG_ITEM_ID));
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_contribution_detail,
+				container, false);
+
+		// Show the dummy content as text in a TextView.
+		if (mItem != null) {
+			((TextView) rootView.findViewById(R.id.contribution_detail))
+					.setText(mItem.content);
+		}
+
+		return rootView;
+	}
+}
